e62588f8be75f5dec2d7fced408246cdc307408c
David Pinto <davidpinto999@gmail.com>
2019-11-17 17:24:54 -0400

    expositors view

--- src/routes/index.js
+++ src/routes/index.js
@@ -112,5 +112,75 @@ router.get('/', (req, res) => res.render(
     }
 ))
 
+const expositors = [
+    [
+        {
+            firstName: 'Isaiah',
+            lastName: 'Anderson',
+            position: 'Vice Presitende Netflix',
+            img: 'img/expositors/image1.jpg',
+            facebook: '#',
+            twitter: '#',
+            instagram: '#'
+        },
+        {
+            firstName: 'Isaiah',
+            lastName: 'Anderson',
+            position: 'Vice Presitende Netflix',
+            img: 'img/expositors/image2.jpg',
+            facebook: '#',
+            twitter: '#',
+            instagram: '#'
+        },
+        {
+            firstName: 'Isaiah',
+            lastName: 'Anderson',
+            position: 'Vice Presitende Netflix',
+            img: 'img/expositors/image3.jpg',
+            facebook: '#',
+            twitter: '#',
+            instagram: '#'
+        },
+    ],
+    [
+        {
+            firstName: 'Isaiah',
+            lastName: 'Anderson',
+            position: 'Vice Presitende Netflix',
+            img: 'img/expositors/image4.jpg',
+            facebook: '#',
+            twitter: '#',
+            instagram: '#'
+        },
+        {
+            firstName: 'Isaiah',
+            lastName: 'Anderson',
+            position: 'Vice Presitende Netflix',
+            img: 'img/expositors/image5.jpg',
+            facebook: '#',
+            twitter: '#',
+            instagram: '#'
+        },
+        {
+            firstName: 'Isaiah',
+            lastName: 'Anderson',
+            position: 'Vice Presitende Netflix',
+            img: 'img/expositors/image0.jpg',
+            facebook: '#',
+            twitter: '#',
+            instagram: '#'
+        },
+    ]
+]
+
+// Expositors Page
+router.get('/expositors', (req, res) => res.render(
+    'expositors/expositors.ejs', 
+    { 
+        title: 'Expositors',
+        expositors
+    }
+))
+
 
 module.exports = router
