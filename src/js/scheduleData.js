1da0e256ee23e6f569b5172d35c8119463450a43
David Pinto <dpinto@jacidi.com>
2020-01-20 10:48:09 -0400

    linkedin + 2 colunm form

--- src/js/scheduleData.js
+++ src/js/scheduleData.js
@@ -8,16 +8,19 @@ const schedule = [
             
                 name: 'Cecilia Roberts',
                 hour: '10.00 - 11.00',
+                room: 'Sala 1',
                 description: 'Plenary Session: Laying the foundations for future success with the new consumer'
             },
             {
                 name: 'Jeff Tate',
                 hour: '10.00 - 11.00',
+                room: 'Sala 1',
                 description: 'Plenary Session: Laying the foundations for future success with the new consumer'
             },
             {
                 name: 'Sean Daniels',
                 hour: '10.00 - 11.00',
+                room: 'Sala 1',
                 description: 'Plenary Session: Laying the foundations for future success with the new consumer'
             }
         ]
@@ -31,16 +34,19 @@ const schedule = [
             
                 name: 'William Hernandez',
                 hour: '10.00 - 11.00',
+                room: 'Sala 1',
                 description: 'Plenary Session: Laying the foundations for future success with the new consumer'
             },
             {
                 name: 'Jerome Martinez',
                 hour: '10.00 - 11.00',
+                room: 'Sala 1',
                 description: 'Plenary Session: Laying the foundations for future success with the new consumer'
             },
             {
                 name: 'Nina Terry',
                 hour: '10.00 - 11.00',
+                room: 'Sala 1',
                 description: 'Plenary Session: Laying the foundations for future success with the new consumer'
             }
         ]
@@ -54,16 +60,19 @@ const schedule = [
             
                 name: 'Marguerite Carlson',
                 hour: '10.00 - 11.00',
+                room: 'Sala 1',
                 description: 'Plenary Session: Laying the foundations for future success with the new consumer'
             },
             {
                 name: 'Eva Norton',
                 hour: '10.00 - 11.00',
+                room: 'Sala 1',
                 description: 'Plenary Session: Laying the foundations for future success with the new consumer'
             },
             {
                 name: 'Ivan Jefferson',
                 hour: '10.00 - 11.00',
+                room: 'Sala 1',
                 description: 'Plenary Session: Laying the foundations for future success with the new consumer'
             }
         ]
