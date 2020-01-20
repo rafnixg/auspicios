a09222b7d4f338ffd5983196f1e46d4645a3a936
David Pinto <davidpinto999@gmail.com>
2020-01-19 23:13:11 -0400

    tabs

--- src/js/expositorsData.js
+++ src/js/expositorsData.js
@@ -1,62 +1,247 @@
-const expositors = [
-    [
-        {
-            firstName: 'Isaiah',
-            lastName: 'Anderson',
-            position: 'Vice Presitende Netflix',
-            img: 'img/expositors/image1.jpg',
-            facebook: '#',
-            twitter: '#',
-            instagram: '#'
-        },
-        {
-            firstName: 'Jeanette',
-            lastName: 'McKinney',
-            position: 'Vice Presitende Netflix',
-            img: 'img/expositors/image2.jpg',
-            facebook: '#',
-            twitter: '#',
-            instagram: '#'
-        },
-        {
-            firstName: 'Ann',
-            lastName: 'Herrera',
-            position: 'Vice Presitende Netflix',
-            img: 'img/expositors/image3.jpg',
-            facebook: '#',
-            twitter: '#',
-            instagram: '#'
-        },
-    ],
-    [
-        {
-            firstName: 'Brent',
-            lastName: 'Ray',
-            position: 'Vice Presitende Netflix',
-            img: 'img/expositors/image4.jpg',
-            facebook: '#',
-            twitter: '#',
-            instagram: '#'
-        },
-        {
-            firstName: 'Jeremy',
-            lastName: 'Robinson',
-            position: 'Vice Presitende Netflix',
-            img: 'img/expositors/image5.jpg',
-            facebook: '#',
-            twitter: '#',
-            instagram: '#'
-        },
-        {
-            firstName: 'Jacob',
-            lastName: 'Saunders',
-            position: 'Vice Presitende Netflix',
-            img: 'img/expositors/image0.jpg',
-            facebook: '#',
-            twitter: '#',
-            instagram: '#'
-        },
-    ]
+const expositors = [{
+        tab: 'keynote',
+        data: [
+            [{
+                    firstName: 'Isaiah',
+                    lastName: 'Anderson',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image1.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+                {
+                    firstName: 'Jeanette',
+                    lastName: 'McKinney',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image2.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+                {
+                    firstName: 'Ann',
+                    lastName: 'Herrera',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image3.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+            ],
+            [{
+                    firstName: 'Brent',
+                    lastName: 'Ray',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image4.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+                {
+                    firstName: 'Jeremy',
+                    lastName: 'Robinson',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image5.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+                {
+                    firstName: 'Jacob',
+                    lastName: 'Saunders',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image0.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+            ]
+        ]
+    },
+    {
+        tab: 'speaker',
+        data: [
+            [{
+                    firstName: 'Brent',
+                    lastName: 'Ray',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image4.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+                {
+                    firstName: 'Jeremy',
+                    lastName: 'Robinson',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image5.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+                {
+                    firstName: 'Jacob',
+                    lastName: 'Saunders',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image0.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+            ],
+            [{
+                    firstName: 'Isaiah',
+                    lastName: 'Anderson',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image1.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+                {
+                    firstName: 'Jeanette',
+                    lastName: 'McKinney',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image2.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+                {
+                    firstName: 'Ann',
+                    lastName: 'Herrera',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image3.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+            ],
+        ]
+    },
+    {
+        tab: 'panelista',
+        data: [
+            [{
+                    firstName: 'Isaiah',
+                    lastName: 'Anderson',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image1.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+                {
+                    firstName: 'Jeanette',
+                    lastName: 'McKinney',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image2.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+                {
+                    firstName: 'Ann',
+                    lastName: 'Herrera',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image3.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+            ],
+            [{
+                    firstName: 'Brent',
+                    lastName: 'Ray',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image4.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+                {
+                    firstName: 'Jeremy',
+                    lastName: 'Robinson',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image5.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+                {
+                    firstName: 'Jacob',
+                    lastName: 'Saunders',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image0.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+            ]
+        ]
+    },
+    {
+        tab: 'moderador',
+        data: [
+            [{
+                    firstName: 'Brent',
+                    lastName: 'Ray',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image4.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+                {
+                    firstName: 'Jeremy',
+                    lastName: 'Robinson',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image5.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+                {
+                    firstName: 'Jacob',
+                    lastName: 'Saunders',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image0.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+            ],
+            [{
+                    firstName: 'Isaiah',
+                    lastName: 'Anderson',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image1.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+                {
+                    firstName: 'Jeanette',
+                    lastName: 'McKinney',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image2.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+                {
+                    firstName: 'Ann',
+                    lastName: 'Herrera',
+                    position: 'Vice Presitende Netflix',
+                    img: 'img/expositors/image3.jpg',
+                    facebook: '#',
+                    twitter: '#',
+                    instagram: '#'
+                },
+            ],
+        ]
+    }
 ]
 
 
